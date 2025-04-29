426689a695cb05754857dfcf1ec0eced623ec9b0
hendr1an <hendriansyahgyan@gmail.com>
2025-04-29 10:57:57 +0700

    menambahkan class

--- introduction.cpp
+++ introduction.cpp
@@ -7,21 +7,5 @@ class mahasiswa{
     int nim;
     string nama;
     float nilai;
-    float nilairatarata;
-
-    void print_date(){
-        cout << "NIM    = " << nim << endl;
-        cout << "Nama    = " << nama << endl;
-        cout << "Nilai    = " << nilai << endl;
-    }
-};
-
-int main(){
-    mahasiswa mhs;
-    mhs.nim = 2023;
-    mhs.nama = "Gyan";
-    mhs.nilai = 99.9;
-
-    mhs.print_date();
 }
-
+ 
